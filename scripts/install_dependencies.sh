aabec7f6235fe404564163ee99d35701fa47f342
Sakshi9111 <chaysakshi911@gmail.com>
2023-06-20 23:07:37 +0545

    add files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
-sudo apt update
-sudo apt install npm -y
-sudo npm install pm2 -g
-# sudo cp /home/ubuntu/server.js  /opt/codedeploy-agent/
-# sudo cp /home/ubuntu/package.json  /opt/codedeploy-agent/
-# sudo cp /home/ubuntu/package-lock.json /opt/codedeploy-agent/
-# sudo cp -R /home/ubuntu/view /opt/codedeploy-agent/
+!/bin/bash
+
+# Move to the directory
+cd /home/ubuntu
+
+# Install dependencies
+npm i
+
+# Build the Code
+npm run build
--- scripts/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm build
-pm2 start /home/ubuntu/server.js
